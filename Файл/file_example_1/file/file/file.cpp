b9dd1e7fa5860fd539291a52b8aed9bf295273d9
hgjgh67 <151984805+hgjgh67@users.noreply.github.com>
2024-01-20 10:19:49 +0300

    Add files via upload

---
+++ Файл/file_example_1/file/file/file.cpp
@@ -0,0 +1,20 @@
+﻿//1//
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	fstream f("D:\\2 КУРС\\Чемель\\Файл\\file1.txt");
+	int j;
+	string surname;
+	int age;
+	for (j = 0; j <= 2; j++)
+	{
+		cout << "Введите фамилию: ";
+		cin >> surname;
+		cout << "Введите возраст: ";
+		cin >> age;
+		f << surname << '\t'<< age << '\n';
+	}
+}
---
+++ Файл/file_example_1/file_example_1/file_example_1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	fstream f("D:\\2 КУРС\\Чемель\\Файл\\file_example_1.txt");
+		int n = 0;
+	for (; !f.eof(); )
+	{
+		char s;
+		f >> s;
+		n++;
+	}
+	cout << "Кол-во символов = " << n << endl;
+}
